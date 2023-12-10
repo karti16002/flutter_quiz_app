014819ece9d2f309fc14bebf1f47db23bf451925
sahil <sahilmehla1234567@gmail.com>
2023-12-10 12:07:27 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,283 +1,140 @@
 import 'package:flutter/material.dart';
 
+// Question model
+class Question {
+  final String question;
+  final List<String> options;
+  final int answer;
+
+  Question({
+    required this.question,
+    required this.options,
+    required this.answer,
+  });
+}
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quiz App',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.blue,
       ),
-      home: QuizScreen(),
-      debugShowCheckedModeBanner: false,
+      home: const QUIZ(),
     );
   }
 }
 
-class QuizScreen extends StatefulWidget {
+class QUIZ extends StatefulWidget {
+  const QUIZ({Key? key}) : super(key: key);
+
   @override
-  _QuizScreenState createState() => _QuizScreenState();
+  State<QUIZ> createState() => _QUIZState();
 }
 
-class _QuizScreenState extends State<QuizScreen> {
-  int _currentIndex = 0;
-  int _score = 0;
-  bool _submitted = false;
-
-  List<Map<String, dynamic>> questions = [
-    {
-      'question': 'What is the capital of France?',
-      'options': ['Berlin', 'Madrid', 'Paris', 'Rome'],
-      'correctIndex': 2,
-    },
-    {
-      'question': 'Which programming language is Flutter based on?',
-      'options': ['Dart', 'Java', 'Python', 'C++'],
-      'correctIndex': 0,
-    },
-    {
-      'question': 'What is the largest mammal in the world?',
-      'options': ['Elephant', 'Blue Whale', 'Giraffe', 'Hippopotamus'],
-      'correctIndex': 1,
-    },
-    {
-      'question': 'Which planet is known as the Red Planet?',
-      'options': ['Mars', 'Venus', 'Jupiter', 'Saturn'],
-      'correctIndex': 0,
-    },
-    {
-      'question': 'Who wrote "Romeo and Juliet"?',
-      'options': [
-        'Charles Dickens',
-        'Jane Austen',
-        'William Shakespeare',
-        'Mark Twain'
-      ],
-      'correctIndex': 2,
-    },
-    {
-      'question': 'What is the capital of Japan?',
-      'options': ['Beijing', 'Seoul', 'Tokyo', 'Bangkok'],
-      'correctIndex': 2,
-    },
-    // Add more questions...
+class _QUIZState extends State<QUIZ> {
+  // List of questions
+  final List<Question> questions = [
+    Question(
+      question: 'Which team won the first Cricket World Cup?',
+      options: ['West-Indies', 'Australia', 'England', 'India'],
+      answer: 0, // Index of the correct answer
+    ),
+    Question(
+      question: 'On which date the Chandrayaan-3 was launched?',
+      options: ['19 July', '23 August', '15 August', '14 July'],
+      answer: 3,
+    ),
+     Question(
+      question: 'What city is known as "The Eternal City"? ',
+      options: ['Tokyo', 'Rome', 'California', 'Paris'],
+      answer: 1,
+    ),
+     Question(
+      question: 'What country has won the most World Cups?',
+      options: ['Brazil', 'Argentina', 'India', 'Portugal'],
+      answer: 0,
+    ),
+     Question(
+      question: 'Which planet has the most moons?',
+      options: ['Uranus', 'Jupiter', 'Saturn', 'Venus'],
+      answer: 3,
+    ),
+    // ... Add more questions ...
   ];
 
-  void _checkAnswer(int selectedIndex) {
-    if (selectedIndex == questions[_currentIndex]['correctIndex']) {
-      setState(() {
+  int _currentQuestionIndex = 0; // Current question index
+  int _score = 0; // Score
+
+  void _checkAnswer(int optionIndex) {
+    if (_currentQuestionIndex < questions.length) {
+      if (optionIndex == questions[_currentQuestionIndex].answer) {
+        // Correct answer
         _score++;
+      } else {
+        // Wrong answer
+      }
+      setState(() {
+        _currentQuestionIndex++;
       });
     }
   }
 
-  void _nextQuestion() {
-    setState(() {
-      if (_currentIndex < questions.length - 1) {
-        _currentIndex++;
-      }
-    });
-  }
-
-  void _previousQuestion() {
-    setState(() {
-      if (_currentIndex > 0) {
-        _currentIndex--;
-      }
-    });
-  }
-
-  void _submitQuiz() {
-    setState(() {
-      _submitted = true;
-    });
-
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Quiz Results'),
-        content: Text('Your Score: $_score / ${questions.length}'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text('Continue', style: TextStyle(color: Colors.teal)),
-          ),
-          TextButton(
-            onPressed: () {
-              _restartQuiz(); // Call _restartQuiz when Restart Quiz is pressed
-              Navigator.pop(context);
-            },
-            child: Text('Restart Quiz', style: TextStyle(color: Colors.teal)),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _restartQuiz() {
-    setState(() {
-      _currentIndex = 0;
-      _score = 0;
-      _submitted = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Quiz App'),
-        backgroundColor: Colors.teal,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.teal,
+    if (_currentQuestionIndex >= questions.length) {
+      return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Quiz Completed!'),
+              Text('Your score: $_score/${questions.length}'),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _currentQuestionIndex = 0;
+                    _score = 0;
+                  });
+                },
+                child: Text('Restart Quiz'),
               ),
-              child: Text(
-                'Quiz App',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              title: Text('Home', style: TextStyle(color: Colors.white)),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            // Add more drawer items...
-          ],
+            ],
+          ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Question ${_currentIndex + 1}/${questions.length}',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.teal,
-              ),
-            ),
-            SizedBox(height: 16),
-            Card(
-              elevation: 3,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Text(
-                      questions[_currentIndex]['question'],
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(height: 16),
-                    ...((questions[_currentIndex]['options'] as List<String>)
-                        .asMap()
-                        .entries
-                        .map(
-                          (entry) => Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: ElevatedButton(
-                              onPressed: _submitted
-                                  ? null
-                                  : () {
-                                      _checkAnswer(entry.key);
-                                      _nextQuestion();
-                                    },
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.white,
-                                onPrimary: Colors.teal,
-                                textStyle: TextStyle(fontSize: 16),
-                              ),
-                              child: Text(entry.value),
-                            ),
-                          ),
-                        )),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if (_currentIndex > 0)
-                  ElevatedButton(
-                    onPressed: () {
-                      _previousQuestion();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.teal,
-                    ),
-                    child:
-                        Text('Previous', style: TextStyle(color: Colors.white)),
-                  ),
-                SizedBox(width: 16),
-                ElevatedButton(
-                  onPressed: _submitted
-                      ? null
-                      : () {
-                          _submitQuiz();
-                        },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.teal,
-                  ),
-                  child: Text('Submit', style: TextStyle(color: Colors.white)),
-                ),
-                SizedBox(width: 16),
-                ElevatedButton(
-                  onPressed: _submitted
-                      ? () {
-                          _restartQuiz();
-                        }
-                      : null,
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.teal,
-                  ),
-                  child: Text('Restart', style: TextStyle(color: Colors.white)),
-                ),
-              ],
-            ),
-            SizedBox(height: 16),
-            if (_submitted)
-              Center(
-                child: Text(
-                  'Your Score: $_score / ${questions.length}',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.teal,
-                  ),
-                ),
-              ),
-          ],
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (!_submitted) {
-            _nextQuestion();
-          }
-        },
-        child: Icon(Icons.navigate_next),
-        backgroundColor: Colors.teal,
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Question ${_currentQuestionIndex + 1}/${questions.length}',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              SizedBox(height: 20.0),
+              Text(questions[_currentQuestionIndex].question),
+              SizedBox(height: 20.0),
+              for (int i = 0; i < questions[_currentQuestionIndex].options.length; i++)
+                ElevatedButton(
+                  onPressed: () => _checkAnswer(i),
+                  child: Text(questions[_currentQuestionIndex].options[i]),
+                ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
